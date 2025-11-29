15e163b1e90f9dc1b181400d03903d5a4cbd485a
patrykharasiuk <patryk.harasiuk@op.pl>
2025-11-30 00:30:12 +0100

    refactor: move game logic to Game class

--- src/main/java/edu/io/Game.java
+++ src/main/java/edu/io/Game.java
@@ -2,20 +2,68 @@ package edu.io;
 
 import edu.io.token.PlayerToken;
 
+import java.util.Scanner;
+
 public class Game {
     private final Board board = new Board();
-
+    private Player player;
 
     public Game() {
         board.spawnGoldTokens(5);
     }
 
     public void join(Player player) {
+        this.player = player;
       new PlayerToken(player, board);
 
     }
 
     public void start() {
-        board.display();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Wpisz: left, right, up, down, none lub exit.");
+        while (true) {
+            board.display();
+
+            System.out.print("Twój ruch: ");
+            String input = sc.nextLine().trim().toLowerCase();
+
+            if ("exit".equals(input)) {
+                System.out.println("Koniec gry");
+                break;
+            }
+
+            processMove(input);
+
+        }
     }
-}
+
+    private void processMove(String move) {
+        PlayerToken.Move parsedMove = parseMove(move);
+        if (parsedMove == null) {
+            System.out.println("Nieznana komenda. Uzyj: left,  right, up, down, none lub exit.");
+            return;
+        }
+
+        try {
+            player.token().move(parsedMove);
+            System.out.println("Zloto: " + player.gold());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Nie mozesz poruszać się dalej w tym kierunku.");
+        }
+
+    }
+
+    private PlayerToken.Move parseMove(String input) {
+        return switch (input) {
+            case "up" -> PlayerToken.Move.UP;
+            case "down" -> PlayerToken.Move.DOWN;
+            case "left" -> PlayerToken.Move.LEFT;
+            case "right" -> PlayerToken.Move.RIGHT;
+            case "none" -> PlayerToken.Move.NONE;
+            default -> null;
+            };
+        }
+    }
+
+
--- src/main/java/edu/io/Main.java
+++ src/main/java/edu/io/Main.java
@@ -1,46 +1,10 @@
 package edu.io;
 
-import edu.io.token.PlayerToken;
-import edu.io.token.PlayerToken.Move;
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Game game = new Game();
         Player player = new Player();
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Wpisz: left, right, up, down, none lub exit.");
-
         game.join(player);
-        while (true) {
-            game.start();
-
-            System.out.print("Twój ruch: ");
-            String input = sc.nextLine().trim().toLowerCase();
-
-            if ("exit".equals(input)) {
-                System.out.println("Koniec gry");
-                break;
-            }
-
-
-            PlayerToken.Move move;
-            switch (input) {
-                case "left":  move = Move.LEFT; break;
-                case "right":  move = Move.RIGHT; break;
-                case "up":  move = Move.UP; break;
-                case "down":  move = Move.DOWN; break;
-                case "none":  move = Move.NONE; break;
-               default: System.out.println("Błędna komenda, spróbuj jeszcze raz"); continue;
-            }
-            try {
-                player.token().move(move);
-            } catch (IllegalArgumentException e) {
-                System.out.println("Nie mozesz poruszać się dalej w tym kierunku.");
-            }
-        }
-        sc.close();
+        game.start();
     }
 }
